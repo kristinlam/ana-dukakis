2faa158d7beef25c3fe0491fc054882a13ba8578
kristinlam <git@github.com:kristinlam/kristinlam.co.git>
2019-05-23 20:58:07 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,15 +1,18 @@
- $(document).ready( function() {
+$(document).ready( function() {
 
 $('.grid').isotope({
+            
   itemSelector: '.grid-item',
+  sortBy: 'random'
 });
 
 // filter items on button click
 $('.filter-button-group').on( 'click', 'li', function() {
   var filterValue = $(this).attr('data-filter');
-  $('.grid').isotope({ filter: filterValue });
-  $('.filter-button-group li').removeClass('active');
-  $(this).addClass('active');
+	  $('.grid').isotope({ filter: filterValue });
+	  $('.filter-button-group li').removeClass('active');
+	  $(this).addClass('active');
 });
 
 })
+
