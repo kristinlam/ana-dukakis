77f39a878269c87845cc4f8bedc7dccb839e3a62
kristinlam <git@github.com:kristinlam/kristinlam.co.git>
2019-05-23 21:13:50 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,35 +1,6 @@
-// $(document).ready( function() {
-
-// $('.grid').isotope({
-            
-//   itemSelector: '.grid-item',
-//   sortBy: 'random'
-// });
-
-// // filter items on button click
-// $('.filter-button-group').on( 'click', 'li', function() {
-//   var filterValue = $(this).attr('data-filter');
-// 	  $('.grid').isotope({ filter: filterValue });
-// 	  $('.filter-button-group li').removeClass('active');
-// 	  $(this).addClass('active');
-// });
-
-// })
-
-
-
-$(document).ready(function(){
-
-
-	// Isotope messes up in Chrome because it initiates before everything has loaded
-
-	// This ensures everything has loaded before applying
-
-	$(window).load(function() {
-
-		$("theselector isotope is on").isotope('reLayout');
-
-		$('.grid').isotope({
+$(document).ready( function() {
+$("theselector isotope is on").isotope('reLayout');
+$('.grid').isotope({
             
   itemSelector: '.grid-item',
   sortBy: 'random'
@@ -43,9 +14,4 @@ $('.filter-button-group').on( 'click', 'li', function() {
 	  $(this).addClass('active');
 });
 
-
-	});
-
-
-});
-
+})
